55f89500b2f74e12b754edf2d98647411c70f2ea
Vinícius Ian <vini.ian@hotmail.com>
2022-11-22 18:17:17 -0300

    working on layout

--- lib/src/pages/contagem.dart
+++ lib/src/pages/contagem.dart
@@ -28,21 +28,32 @@ class _ContagemPageState extends State<ContagemPage> {
         if (snapshot.hasData) {
           List<ContagemPendentes> contagem = snapshot.data;
           List<DataRow> listaLotes = [];
-          print(contagem.length);
+          int tamanhoLista = contagem.length;
+          List<TextEditingController> unidadesExistentes = [];
+          List<TextEditingController> caixasExistentes = [];
+          List<int> total = [];
+          for (var i = 0; i < tamanhoLista; i++) {
+            unidadesExistentes.add(TextEditingController());
+            caixasExistentes.add(TextEditingController());
 
-          for (var produtos in contagem) {
             listaLotes.add(DataRow(cells: [
               DataCell(Center(
-                child: Text('${produtos.lote}'),
+                child: Text('${contagem[i].lote}'),
               )),
               DataCell(Center(
-                child: Text('${produtos.validade}'),
+                child: Text('${contagem[i].validade}'),
               )),
               DataCell(Center(
-                child: TextFormField(),
+                child: TextFormField(
+                  controller: caixasExistentes[i],
+                  keyboardType: TextInputType.number,
+                ),
               )),
               DataCell(Center(
-                child: TextFormField(),
+                child: TextFormField(
+                  controller: unidadesExistentes[i],
+                  keyboardType: TextInputType.number,
+                ),
               )),
               DataCell(Center(
                 child: TextFormField(),
@@ -54,185 +65,195 @@ class _ContagemPageState extends State<ContagemPage> {
             body: LayoutBuilder(
               builder: (context, constraints) => SizedBox(
                   width: double.infinity,
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.center,
-                    children: [
-                      SizedBox(
-                        height: 30,
-                      ),
-                      Text('CONTADOR: ${widget.nome} ', style: textoDesc),
-                      SizedBox(
-                        height: 20,
-                      ),
-                      Divider(
-                        thickness: 6.0,
-                      ),
-                      SizedBox(
-                        height: 20,
-                      ),
-                      SizedBox(
-                        height: 20,
-                      ),
-                      Text('ENDEREÇO', style: textoDesc),
-                      Row(
-                        children: [
-                          Expanded(
-                            child: SingleChildScrollView(
-                              child: DataTable(columns: [
-                                DataColumn(
-                                    label: Expanded(
-                                        child: Text(
-                                  'Depósito',
-                                  textAlign: TextAlign.center,
-                                ))),
-                                DataColumn(
-                                    label: Expanded(
-                                        child: Text(
-                                  'Rua',
-                                  textAlign: TextAlign.center,
-                                ))),
-                                DataColumn(
-                                    label: Expanded(
-                                        child: Text(
-                                  'Bloco',
-                                  textAlign: TextAlign.center,
-                                ))),
-                                DataColumn(
-                                    label: Expanded(
-                                        child: Text(
-                                  'Nivel',
-                                  textAlign: TextAlign.center,
-                                ))),
-                                DataColumn(
-                                    label: Expanded(
-                                        child: Text(
-                                  'Apartamento',
-                                  textAlign: TextAlign.center,
-                                ))),
-                              ], rows: [
-                                DataRow(cells: [
-                                  DataCell(Center(
-                                      child: Text('${contagem[0].deposito}'))),
-                                  DataCell(Center(
-                                      child: Text('${contagem[0].rua}'))),
-                                  DataCell(Center(
-                                      child: Text('${contagem[0].bloco}'))),
-                                  DataCell(Center(
-                                      child: Text('${contagem[0].nivel}'))),
-                                  DataCell(Center(
-                                      child:
-                                          Text('${contagem[0].apartamento}')))
-                                ])
-                              ]),
+                  child: SingleChildScrollView(
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      children: [
+                        SizedBox(
+                          height: 30,
+                        ),
+                        Text('CONTADOR: ${widget.nome} ', style: textoDesc),
+                        SizedBox(
+                          height: 20,
+                        ),
+                        Divider(
+                          thickness: 6.0,
+                        ),
+                        SizedBox(
+                          height: 20,
+                        ),
+                        SizedBox(
+                          height: 20,
+                        ),
+                        Text('ENDEREÇO', style: textoDesc),
+                        Row(
+                          children: [
+                            Expanded(
+                              child: SingleChildScrollView(
+                                child: DataTable(columns: [
+                                  DataColumn(
+                                      label: Expanded(
+                                          child: Text(
+                                    'Depósito',
+                                    textAlign: TextAlign.center,
+                                  ))),
+                                  DataColumn(
+                                      label: Expanded(
+                                          child: Text(
+                                    'Rua',
+                                    textAlign: TextAlign.center,
+                                  ))),
+                                  DataColumn(
+                                      label: Expanded(
+                                          child: Text(
+                                    'Bloco',
+                                    textAlign: TextAlign.center,
+                                  ))),
+                                  DataColumn(
+                                      label: Expanded(
+                                          child: Text(
+                                    'Nivel',
+                                    textAlign: TextAlign.center,
+                                  ))),
+                                  DataColumn(
+                                      label: Expanded(
+                                          child: Text(
+                                    'Apartamento',
+                                    textAlign: TextAlign.center,
+                                  ))),
+                                ], rows: [
+                                  DataRow(cells: [
+                                    DataCell(Center(
+                                        child:
+                                            Text('${contagem[0].deposito}'))),
+                                    DataCell(Center(
+                                        child: Text('${contagem[0].rua}'))),
+                                    DataCell(Center(
+                                        child: Text('${contagem[0].bloco}'))),
+                                    DataCell(Center(
+                                        child: Text('${contagem[0].nivel}'))),
+                                    DataCell(Center(
+                                        child:
+                                            Text('${contagem[0].apartamento}')))
+                                  ])
+                                ]),
+                              ),
                             ),
-                          ),
-                        ],
-                      ),
-                      SizedBox(
-                        height: 20,
-                      ),
-                      Divider(
-                        thickness: 6.0,
-                      ),
-                      SizedBox(
-                        height: 20,
-                      ),
-                      Text(
-                        'PRODUTO',
-                        style: textoDesc,
-                      ),
-                      Row(
-                        children: [
-                          Expanded(
-                            child: SingleChildScrollView(
-                              child: DataTable(columns: [
-                                DataColumn(
-                                    label: Expanded(
-                                        child: Text(
-                                  'SKU',
-                                  textAlign: TextAlign.center,
-                                ))),
-                                DataColumn(
-                                    label: Expanded(
-                                        child: Text(
-                                  'Descrição',
-                                  textAlign: TextAlign.center,
-                                ))),
-                                DataColumn(
-                                    label: Expanded(
-                                        child: Text(
-                                  'Fator Caixa',
-                                  textAlign: TextAlign.center,
-                                ))),
-                              ], rows: [
-                                DataRow(cells: [
-                                  DataCell(Center(
-                                      child: Text('${contagem[0].cod}'))),
-                                  DataCell(Center(
-                                      child: Text('${contagem[0].descricao}'))),
-                                  DataCell(Center(
-                                      child:
-                                          Text('${contagem[0].fatorCaixa}'))),
-                                ])
-                              ]),
+                          ],
+                        ),
+                        SizedBox(
+                          height: 20,
+                        ),
+                        Divider(
+                          thickness: 6.0,
+                        ),
+                        SizedBox(
+                          height: 20,
+                        ),
+                        Text(
+                          'PRODUTO',
+                          style: textoDesc,
+                        ),
+                        Row(
+                          children: [
+                            Expanded(
+                              child: SingleChildScrollView(
+                                child: DataTable(columns: [
+                                  DataColumn(
+                                      label: Expanded(
+                                          child: Text(
+                                    'SKU',
+                                    textAlign: TextAlign.center,
+                                  ))),
+                                  DataColumn(
+                                      label: Expanded(
+                                          child: Text(
+                                    'Descrição',
+                                    textAlign: TextAlign.center,
+                                  ))),
+                                  DataColumn(
+                                      label: Expanded(
+                                          child: Text(
+                                    'Fator Caixa',
+                                    textAlign: TextAlign.center,
+                                  ))),
+                                ], rows: [
+                                  DataRow(cells: [
+                                    DataCell(Center(
+                                        child: Text('${contagem[0].cod}'))),
+                                    DataCell(Center(
+                                        child:
+                                            Text('${contagem[0].descricao}'))),
+                                    DataCell(Center(
+                                        child:
+                                            Text('${contagem[0].fatorCaixa}'))),
+                                  ])
+                                ]),
+                              ),
                             ),
-                          ),
-                        ],
-                      ),
-                      SizedBox(
-                        height: 20,
-                      ),
-                      Divider(
-                        thickness: 6.0,
-                      ),
-                      SizedBox(
-                        height: 20,
-                      ),
-                      Text(
-                        'PRODUTO',
-                        style: textoDesc,
-                      ),
-                      Row(
-                        children: [
-                          Expanded(
-                            child: SingleChildScrollView(
-                              child: DataTable(columns: [
-                                DataColumn(
-                                    label: Expanded(
-                                        child: Text(
-                                  'Lote',
-                                  textAlign: TextAlign.center,
-                                ))),
-                                DataColumn(
-                                    label: Expanded(
-                                        child: Text(
-                                  'Validade',
-                                  textAlign: TextAlign.center,
-                                ))),
-                                DataColumn(
-                                    label: Expanded(
-                                        child: Text(
-                                  'Caixa',
-                                  textAlign: TextAlign.center,
-                                ))),
-                                DataColumn(
-                                    label: Expanded(
-                                        child: Text(
-                                  'Unidade',
-                                  textAlign: TextAlign.center,
-                                ))),
-                                DataColumn(
-                                    label: Expanded(
-                                        child: Text(
-                                  'Total',
-                                  textAlign: TextAlign.center,
-                                ))),
-                              ], rows: listaLotes),
+                          ],
+                        ),
+                        SizedBox(
+                          height: 20,
+                        ),
+                        Divider(
+                          thickness: 6.0,
+                        ),
+                        SizedBox(
+                          height: 20,
+                        ),
+                        Text(
+                          'CONTAGEM',
+                          style: textoDesc,
+                        ),
+                        Row(
+                          children: [
+                            Expanded(
+                              child: SingleChildScrollView(
+                                child: DataTable(columns: [
+                                  DataColumn(
+                                      label: Expanded(
+                                          child: Text(
+                                    'Lote',
+                                    textAlign: TextAlign.center,
+                                  ))),
+                                  DataColumn(
+                                      label: Expanded(
+                                          child: Text(
+                                    'Validade',
+                                    textAlign: TextAlign.center,
+                                  ))),
+                                  DataColumn(
+                                      label: Expanded(
+                                          child: Text(
+                                    'Caixa',
+                                    textAlign: TextAlign.center,
+                                  ))),
+                                  DataColumn(
+                                      label: Expanded(
+                                          child: Text(
+                                    'Unidade',
+                                    textAlign: TextAlign.center,
+                                  ))),
+                                  DataColumn(
+                                      label: Expanded(
+                                          child: Text(
+                                    'Total',
+                                    textAlign: TextAlign.center,
+                                  ))),
+                                ], rows: listaLotes),
+                              ),
                             ),
-                          ),
-                        ],
-                      ),
-                    ],
+                          ],
+                        ),
+                        SizedBox(height: 20,),
+                        ElevatedButton(
+                            onPressed: () {
+                              print(caixasExistentes[0].text);
+                            },
+                            child: Text('ENVIAR'))
+                      ],
+                    ),
                   )),
             ),
           );
